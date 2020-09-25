bb9bdd297593377a7cf58538b299aee202424aaa
JGamez17 <github email address>
2020-09-24 23:16:14 -0400

    WIP: created raffles controler and views for index, new, and show. Tested database and ensured connected and working properly

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,3 @@
 class DonationsController < ApplicationController
+    
 end
--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,2 +1,47 @@
 class RafflesController < ApplicationController
+    def index
+        @raffles = Raffle.all 
+    end
+
+    def new 
+        @raffle = Raffle.new
+    end
+
+    def show
+        @raffle = Raffle.find_by_id(params[:id])
+    end
+
+    def create 
+        @raffle = Raffle.new(raffle_params)
+        if @raffle.save
+            redirect_to raffle_path(@raffle)
+        else render :new
+        end
+    end
+
+    def edit 
+        @raffle = Raffle.find_by_id(params[:id])
+    end
+
+    def update
+        @raffle = Raffle.find_by_id(params[:id])
+        if @raffle.update(raffle_params)
+             redirect_to raffle_path(@raffle)
+        else render :edit
+        end
+    end
+
+    def destroy 
+        @raffle = Raffle.find_by_id(params[:id])
+        if @raffle.delete 
+            redirect_to "homepage"
+        else redirect_to raffle_path(@raffle)
+        end
+    end
+
+    private
+        def raffle_params
+            params.require(:raffle).permit(:ticket_price, :name_of_raffle)
+        end
+
 end
