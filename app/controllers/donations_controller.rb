771b77138f1228ffe7e2f69a60fefa83a5b6e05b
JGamez17 <github email address>
2020-09-28 15:32:59 -0400

    WIP: finsihed VIEWS for raffle model

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,3 +1,31 @@
 class DonationsController < ApplicationController
-    
+
+    def index
+        @donations = Donations.all 
+    end
+
+    def new 
+        # instantiate raffle based on params[:raffle_id]
+        @donation = Donation.new
+    end
+
+    def show
+        @donation = Donation.find_by_id(params[:id])
+    end
+
+    def create 
+        @raffle = Raffle.new(params)
+        # instantiate raffle based on params[:raffle_id]
+        # donation_params.merge(params[:raffle_id])
+        @donation = current_user.donations.new(donation_params)
+        if @donation.save
+            redirect_to donation_path(@donation)
+        else render :new
+        end
+    end
+
+    private
+        def donation_params
+            params.require(:donation).permit(:number_of_tickets)
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :donations
     has_many :raffles, through: :donations
     has_secure_password
+    validates :username, uniqueness :true 
 end
